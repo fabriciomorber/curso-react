b7384c26194ec339874635ed7dd64b748daa3487
Fabricio Bertrand <fabmorber@gmail.com>
2025-04-15 16:13:15 -0300

    Aula 02

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,14 +1,16 @@
-import {Button} from "./components/Button.tsx";
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button.tsx";
+import {defaultTheme} from "./styles/themes/default.ts";
 
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
